05170b539291eefc1d95d364608f06b15653c848
prateekstark <gargprateek35@gmail.com>
2019-10-09 16:53:53 +0530

    added literals

--- main.cpp
+++ main.cpp
@@ -24,12 +24,19 @@ public:
 		}
 		cout<<endl;
 	}
+	bool doesAdjacencyListContain(int toVertex){
+		for(int i=0;i<fromAdjacencyList.size();i++){
+			if(fromAdjacencyList.at(i) == toVertex){
+				return true;
+			}
+		}
+		return false;
+	}
 };
 
 class Graph{
 public:
 	map<int, Node*> vertexMap;
-
 	void addEdge(int from, int to){
 		if(vertexMap.find(from) == vertexMap.end()){
 			Node* newNode = new Node(from);
@@ -75,6 +82,25 @@ vector<int> extractEdgeInformation(string line){
 	return answer;
 }
 
+int cnfCoordinate(int x, int y, int n, int m){
+	return (m*(x - 1) + y);
+}
+
+void print2DVector(vector<vector<int> > v){
+	for(int i=0;i<v.size();i++){
+		for(int j=0;j<v.at(i).size();j++){
+			cout << v.at(i).at(j) << " ";
+		}
+		cout<<endl;
+	}
+}
+
+pair<int, int> cnfToCartesian(int cnfCoordinate, int n, int m){
+	int x = (cnfCoordinate - 1)/m + 1;
+	int y = (cnfCoordinate - 1)%m + 1;
+	return pair<int, int>(x, y);
+}
+
 int main(){
 	int from, to;
 	Graph largerGraph;
@@ -99,8 +125,62 @@ int main(){
 			}
 		}
 	}
+	int n = smallerGraph.vertexMap.size();
+	int m = largerGraph.vertexMap.size();
+	bool mapMatrix[n][m];
+	cout << n << " " << m << endl;
+	vector<vector<int> > cnfInput;
 
-	largerGraph.printGraph();
-	smallerGraph.printGraph();
+	int numClauses;
+
+	for(int i=1; i <= n; i++){
+		vector<int> tempVector;
+		for(int j=1; j <= m-1; j++){
+			for(int k=j+1; k <= m; k++){
+				tempVector.clear();
+				tempVector.push_back(-cnfCoordinate(i, j, n, m));
+				tempVector.push_back(-cnfCoordinate(i, k, n, m));
+				cnfInput.push_back(tempVector);
+			}
+		}
+		tempVector.clear();
+		for(int j = 1; j<= m; j++){
+			tempVector.push_back(cnfCoordinate(i, j, n, m));
+		}
+		cnfInput.push_back(tempVector);
+	}
+
+	for(int i=1; i <= m; i++){
+		vector<int> tempVector;
+		for(int j=1; j <= n-1; j++){
+			for(int k=j+1; k <= n; k++){
+				tempVector.clear();
+				tempVector.push_back(-cnfCoordinate(j, i, n, m));
+				tempVector.push_back(-cnfCoordinate(k, i, n, m));
+				cnfInput.push_back(tempVector);
+			}
+		}
+	}
+
+	for(int i=1; i <= n; i++){
+		vector<int> tempVector;
+		for(int j=1; j <= n; j++){
+			for(int k=1; k <= m; k++){
+				for(int l=1; l <= m; l++){
+					if(i != j && k != l){
+						tempVector.clear();
+						if(smallerGraph.vertexMap[i]->doesAdjacencyListContain(j) != largerGraph.vertexMap[k]->doesAdjacencyListContain(l)){
+							tempVector.push_back(-cnfCoordinate(i, k, n, m));
+							tempVector.push_back(-cnfCoordinate(j, l, n, m));
+							cnfInput.push_back(tempVector);
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+	print2DVector(cnfInput);
 	return 0;
 }
