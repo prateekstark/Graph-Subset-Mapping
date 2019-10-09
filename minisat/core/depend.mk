1f6073f5cf0dbf72971c3975dd74d8e83fdcb967
prateekstark <gargprateek35@gmail.com>
2019-10-09 11:44:22 +0530

    adding minisat

---
+++ minisat/core/depend.mk
@@ -0,0 +1,28 @@
+/home/stark/Projects/Graph-Subset-Mapping/minisat/core/Solver.o /home/stark/Projects/Graph-Subset-Mapping/minisat/core/Solver.or /home/stark/Projects/Graph-Subset-Mapping/minisat/core/Solver.od /home/stark/Projects/Graph-Subset-Mapping/minisat/core/Solver.op: \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/core/Solver.cc \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/mtl/Sort.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/mtl/Vec.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/mtl/IntTypes.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/mtl/XAlloc.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/core/Solver.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/mtl/Heap.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/mtl/Alg.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/utils/Options.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/utils/ParseUtils.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/core/SolverTypes.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/mtl/Map.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/mtl/Alloc.h
+/home/stark/Projects/Graph-Subset-Mapping/minisat/core/Main.o /home/stark/Projects/Graph-Subset-Mapping/minisat/core/Main.or /home/stark/Projects/Graph-Subset-Mapping/minisat/core/Main.od /home/stark/Projects/Graph-Subset-Mapping/minisat/core/Main.op: /home/stark/Projects/Graph-Subset-Mapping/minisat/core/Main.cc \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/utils/System.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/mtl/IntTypes.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/utils/ParseUtils.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/utils/Options.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/mtl/Vec.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/mtl/XAlloc.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/core/Dimacs.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/core/SolverTypes.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/mtl/Alg.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/mtl/Map.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/mtl/Alloc.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/core/Solver.h \
+ /home/stark/Projects/Graph-Subset-Mapping/minisat/mtl/Heap.h
